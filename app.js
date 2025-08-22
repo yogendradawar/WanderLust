8ce1351a23267f2bfe2395b61790a4612c95317a
Yogendra <yogendradawar6@gmail.com>
2025-08-22 07:54:06 +0530

    update initial

--- app.js
+++ app.js
@@ -80,6 +80,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("/",(req,res) =>{
+   res.redirect("/listings")
+})
 
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
